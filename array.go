dc3af6f89b4cedaba34ff2d6ea2163b6f03021c2
Misly <60405462+Misly16@users.noreply.github.com>
2020-10-15 19:26:54 +0100

    Create array.go

---
+++ array.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	// The array
+	var nameArray [2]string
+
+	// Values
+	nameArray[0] = "John"
+	nameArray[1] = "Alex"
+	// Print the array
+	fmt.Println(nameArray)
+	fmt.Println("\nThe first name is",nameArray[0] )
+	fmt.Println("\nThe second name is",nameArray[1] )
+}
